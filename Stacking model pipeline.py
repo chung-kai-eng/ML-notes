12a92b761031d9a764dd5046fdccd01dcd3b86da
chung-kai-eng <changchungkai0606@gmail.com>
2022-06-01 10:55:18 +0800

    stacking model

---
+++ Stacking model pipeline.py
@@ -0,0 +1,47 @@
+from sklearn.compose import make_column_transformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder, StandardScaler
+from sklearn.linear_model import LassoCV, RidgeCV
+from sklearn.ensemble import RandomForestRegressor, HistGradientBoostingRegressor, StackingRegressor
+
+num_feats = []
+cat_feats = []
+
+
+cat_tree_processor = OrdinalEncoder(
+    handle_unknown="use_encoded_value", unknown_value=-1
+)
+num_tree_processor = SimpleImputer(strategy="mean", add_indicator=True)
+
+tree_preprocessor = make_column_transformer(
+    (num_tree_processor, num_feats), (cat_tree_processor, cat_feats)
+)
+
+
+cat_linear_processor = OneHotEncoder(handle_unknown="ignore")
+num_linear_processor = make_pipeline(
+    StandardScaler(), SimpleImputer(strategy="mean", add_indicator=True)
+)
+
+linear_preprocessor = make_column_transformer(
+    (num_linear_processor, num_feats), (cat_linear_processor, cat_feats)
+)
+
+# 分別建構 base learner
+lasso_pipeline = make_pipeline(linear_preprocessor, LassoCV())
+rf_pipeline = make_pipeline(
+    tree_preprocessor, RandomForestRegressor(random_state=42))
+gbdt_pipeline = make_pipeline(
+    tree_preprocessor, HistGradientBoostingRegressor(random_state=0)
+)
+
+
+estimators = [
+    ("Random Forest", rf_pipeline),
+    ("Lasso", lasso_pipeline),
+    ("Gradient Boosting", gbdt_pipeline),
+]
+
+stacking_regressor = StackingRegressor(
+    estimators=estimators, final_estimator=RidgeCV())
